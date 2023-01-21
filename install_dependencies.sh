6d1c308b94aa2d6eafaa640e69451776a49e38ea
Balder W. Holst <balderwh@gmail.com>
2023-01-22 00:43:37 +0100

    can maybe actually install dependencies now...

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,6 @@
 
 packages="$(curl -s "https://raw.githubusercontent.com/BalderHolst/fedora-jonaburg-picom/main/packages.txt")"
 
-sudo dnf install $packages
+echo -e "\nI need sudo privleges to install the dependencies. Please check the script before typing your passcode."
+
+sudo dnf install $packages -y
